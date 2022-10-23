70fc8844acd3c18eed05da9770ff8f287970c9ec
arceus1410 <115806698+arceus1410@users.noreply.github.com>
2022-10-23 22:49:53 +0700

    Add files via upload

---
+++ Array/exe152.cpp
@@ -0,0 +1,68 @@
+//Find two duplicate elements in a limited range array (using XOR)
+//Given an integer array of size n+2 containing elements between 1 and n with two element repeating, find both duplicate elements without using any extra memory in linear time.
+//Input:  arr[] = [4, 3, 6, 5, 2, 4, 1, 1]
+//Output: The duplicate elements are 1 and 4
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Function to find two repeating elements in an array of size `n+2`
+// having a range of elements from 1 to `n` using XOR operator
+pair<int, int> findDuplicates(int arr[], int n)
+{
+	// take XOR of all array elements from index 0 to `n-1` and
+	// all numbers in range 1 to `n`
+	int result = arr[0] ^ arr[n+1];
+	for (int i = 1; i <= n; i++) {
+		result = result ^ arr[i] ^ i;
+	}
+
+	// `x` and `y` are two duplicate elements
+	int x = 0, y = 0;
+
+	// `result` stores `x ^ y`
+
+	// find the position of the rightmost set bit in `result`
+	int k = log2(result & -result);
+
+	// split the array into two subarrays
+	for (int i = 0; i < n + 2; i++)
+	{
+		// array elements that have k'th bit 1
+		if (arr[i] & (1 << k)) {
+			x = x ^ arr[i];
+		}
+
+		// array elements that have k'th bit 0
+		else {
+			y = y ^ arr[i];
+		}
+	}
+
+	// split range [1, n] into two subranges
+	for (int i = 1; i <= n; i++)
+	{
+		// number `i` has k'th bit 1
+		if (i & (1 << k)) {
+			x = x ^ i;
+		}
+
+		// number `i` has k'th bit 0
+		else {
+			y = y ^ i;
+		}
+	}
+
+	return pair(x, y);
+}
+
+int main()
+{
+	int arr[] = { 4, 3, 6, 5, 2, 4, 1, 1 };
+	int n = 6;		// array size is `n+2`
+
+	pair<int, int> p = findDuplicates(arr, n);
+	cout << "The duplicate elements are " << p.first << " and " << p.second;
+
+	return 0;
+}
